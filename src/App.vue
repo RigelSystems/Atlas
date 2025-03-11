dc075d4d893bddf1362e2782005721c8e6814f29
Alex Ford <alex96ford19@gmail.com>
2025-03-11 15:55:42 +0000

    Basic layout from NovaUI

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,37 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <NNavigationBar :links="links"/>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <NHeroSection title="Hero Section Title" subtitle="Hero Section Subtitle" content="Some example text for this hero section" :actionButtons="[{label: 'Action Button', url: '/home'}]" />
 
-  <main>
-    <TheWelcome />
-  </main>
+  <NRow :cols="{sm: [100], md: [60,40,40,60], lg: [33.33,33.33,33.33]}">
+    <NCard title="Card Title" subtitle="Card Subtitle" content="Some example text for this card" :actionButtons="[{label: 'Action Button', url: '/home'}]" />
+    <NCard title="Card Title" subtitle="Card Subtitle" content="Some example text for this card" :actionButtons="[{label: 'Action Button', url: '/home'}]" />
+    <NCard title="Card Title" subtitle="Card Subtitle" content="Some example text for this card" :actionButtons="[{label: 'Action Button', url: '/home'}]" />
+    <NCard title="Card Title" subtitle="Card Subtitle" content="Some example text for this card" :actionButtons="[{label: 'Action Button', url: '/home'}]" />
+    <NCard title="Card Title" subtitle="Card Subtitle" content="Some example text for this card" :actionButtons="[{label: 'Action Button', url: '/home'}]" />
+    <NCard title="Card Title" subtitle="Card Subtitle" content="Some example text for this card" :actionButtons="[{label: 'Action Button', url: '/home'}]" />
+  </NRow>
+
+  <NRow :cols="{sm: [100], md: [33.3,33.3,33.3]}">
+    <NDropdown label="Click Me" content="Some hidden information!" />
+    <NDropdown label="Don't click me!" content="What did I say..." />
+    <NDropdown label="Ummmmm" content="Wow, you're an explorer!" />
+  </NRow>
+
+  <h2>Contact Form</h2>
+  <p>Please enter your details below and we'll contact you shortly!</p>
+
+  <NRow :cols="{sm: [100], md: [50,50,100,100]}">
+    <NTextInput label="First Name" placeholder="John" />
+    <NTextInput label="Last Name" placeholder="Doe" />
+    <NTextInput label="Email" placeholder="Enter your email address" />
+    <NTextInput label="Message" placeholder="Enter your message here" />
+    <NButton label="Submit" />
+  </NRow>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import NovaUI from '@rigelsystems/novaui'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(NovaUI, { theme: '#000', borderRadius: '5px' });
+app.mount('#app')
